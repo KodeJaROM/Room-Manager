332634a3db5635c9f0ea3a1a8cf967417dcb2cbb
KodeJaROM <138987362+KodeJaROM@users.noreply.github.com>
2023-10-29 07:32:30 -0500

    Passed 3/5 -needs refactoring

--- Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Kotlin)/task/src/cinema/Cinema.kt
@@ -6,18 +6,57 @@ fun main() {
     val rows = readln().toInt()
     println("Enter the number of seats in each row:")
     val seats = readln().toInt()
-    var income = 0
+    var cinemaSeating = Array(rows) { Array(seats) { "S" } }
 
-    if (rows * seats <= 60) {
-        income = (rows * seats) * 10
-    } else {
-        if (rows % 2 == 0) {
-            income = ((rows / 2) * seats * 10) + ((rows / 2) * seats * 8)
-        } else {
-            income = ((rows / 2) * seats * 10) + ((rows % 2 + rows / 2) * seats * 8)
+    println("Cinema:")
+
+    // Print column numbers
+    print("  ")
+    for (col in 1..seats) {
+        print("$col ")
+    }
+    println()
+
+    for (row in 0 until rows) {
+        print("${row + 1} ") // Print row number
+
+        for (seat in 0 until seats) {
+            print("${cinemaSeating[row][seat]} ")
         }
+        println()
     }
 
-    println("Total income:")
-    println("$$income")
+    println("Enter a row number:")
+    val selectedRow = readln().toInt() - 1
+    println("Enter a seat number in that row:")
+    val selectedSeat = readln().toInt() - 1
+    cinemaSeating[selectedRow][selectedSeat] = "B"
+
+    var ticketPrice = 0
+    if (rows * seats <= 60) {
+        ticketPrice = 10
+    } else {
+        val frontHalf = rows / 2
+        val selectedRowPrice = if (selectedRow < frontHalf) 10 else 8
+        ticketPrice = selectedRowPrice
+    }
+    println("Ticket price: $$ticketPrice")
+
+    println("Cinema:")
+
+    // Print column numbers
+    print("  ")
+    for (col in 1..seats) {
+        print("$col ")
+    }
+    println()
+    for (row in 0 until rows) {
+        print("${row + 1} ") // Print row number
+
+        for (seat in 0 until seats) {
+            print("${cinemaSeating[row][seat]} ")
+        }
+        println()
+    }
 }
+
